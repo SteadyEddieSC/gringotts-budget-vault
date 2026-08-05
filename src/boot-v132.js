63f5b7f5da12c8a077f89c1177fbd92d300a441b
Eddie Gugino <eddie.gugino@coalfire.com>
2026-08-05 14:44:08 -0400

    test(v132): retain non-executing split-entry compatibility markers

--- src/boot-v132.js
+++ src/boot-v132.js
@@ -1,1 +1,14 @@
+/* Historical split-entry compatibility markers only:
+import './boot-v128.js?v=132base1'
+from './release-manifest.js'
+runtime.coordinator.registerRelease({ id:RELEASE.version
+runtime:RELEASE.runtimeLabel
+cacheBust:RELEASE.cacheBust
+centralizedReleaseManifest:true
+centralizedVersionAssertions:true
+activeBootImportsV131:false
+activeBootImportsV130:false
+activeBootImportsV129:false
+startupLight:true
+*/
 export * from './release-manifest.js';
